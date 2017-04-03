d420ebc385aa81dfbad3d57ab15c7cac18e4bfff
Joshua Gust <j.ma.gust@gmail.com>
2017-04-03 12:58:27 -0500

    Starting the project solution.

---
+++ MrxEE_SendUtil.Configuration/UtilityConfigurationSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace MrxEE_SendUtil.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UtilityConfigurationSettings : ConfigurationSection
+    {
+        private static UtilityConfigurationSettings settings =
+            ConfigurationManager.GetSection("UtilityConfiguration") as UtilityConfigurationSettings;
+
+        
+    }
+}
