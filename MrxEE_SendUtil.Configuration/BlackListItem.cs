557079b52ad2dee2e6cc50dfe04724cd7a2d7d70
Joshua Gust <j.ma.gust@gmail.com>
2017-10-27 14:58:26 -0500

    Touch up the configuration classes. Documentation. Add xml schema.

---
+++ MrxEE_SendUtil.Configuration/BlackListItem.cs
@@ -0,0 +1,44 @@
+﻿namespace MrxEE_SendUtil.Configuration
+{
+    using System.Configuration;
+
+    /// <summary>
+    /// The black list item.
+    /// </summary>
+    public class BlackListItem : ConfigurationElement
+    {
+        /// <summary>
+        /// Gets or sets the group id.
+        /// </summary>
+        [ConfigurationProperty("groupId")]
+        public string GroupId
+        {
+            get
+            {
+                return base["groupId"] as string;
+            }
+
+            set
+            {
+                base["groupId"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the group name.
+        /// </summary>
+        [ConfigurationProperty("groupName")]
+        public string GroupName
+        {
+            get
+            {
+                return base["groupName"] as string;
+            }
+
+            set
+            {
+                base["groupName"] = value;
+            }
+        }
+    }
+}
--- MrxEE_SendUtil.Configuration/UtilityConfigurationSettings.cs
+++ MrxEE_SendUtil.Configuration/UtilityConfigurationSettings.cs
@@ -1,17 +1,53 @@
 ﻿namespace MrxEE_SendUtil.Configuration
 {
-    using System;
     using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    public class UtilityConfigurationSettings : ConfigurationSection
+    /// <summary>
+    /// The utility configuration section.
+    /// </summary>
+    public class UtilityConfigurationSection : ConfigurationSection
     {
-        private static UtilityConfigurationSettings settings =
-            ConfigurationManager.GetSection("UtilityConfiguration") as UtilityConfigurationSettings;
+        /// <summary>
+        /// Gets the settings.
+        /// </summary>
+        public static UtilityConfigurationSection Settings { get; } = ConfigurationManager.GetSection("UtilityConfiguration") as UtilityConfigurationSection;
 
-        
+        /// <summary>
+        /// Gets or sets the blacklist.
+        /// </summary>
+        [ConfigurationProperty("blacklist")]
+        public GroupBlacklist Blacklist
+        {
+            get
+            {
+                return base["blacklist"] as GroupBlacklist;
+            }
+
+            set
+            {
+                base["blacklist"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the input output settings.
+        /// </summary>
+        [ConfigurationProperty("input_output")]
+        public InputOutput InputOutputSettings
+        {
+            get
+            {
+                return base["input_output"] as InputOutput;
+            }
+
+            set
+            {
+                base["input_output"] = value;
+            }
+        }
     }
 }
